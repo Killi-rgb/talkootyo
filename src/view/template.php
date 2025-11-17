e2443657ecfa1fdfadb9b11a0b9bf1ffbfc3d333
Janne Stolt <p50456@edu.sasky.fi>
2025-11-17 10:46:49 +0200

    aloitus tilanne

---
+++ src/view/template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <title>lanify - <?=$this->e($title)?></title>
+    <meta charset="UTF-8">
+    <link href="styles/styles.css" rel="stylesheet">  
+  </head>
+  <body>
+    <header>
+      <h1><a href="<?=BASEURL?>">lanify</a></h1>
+          <header>
+       
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+            if (isset($_SESSION['admin']) && $_SESSION['admin']) {
+              echo "<div><a href='admin'>Ylläpitosivut</a></div>";  
+            }
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
+    </header>
+
+    </header>
+    <section>
+      <?=$this->section('content')?>
+    </section>
+    <footer>
+      <hr>
+      <div>lanify by Kurpitsa</div>
+    </footer>
+  </body>
+</html>
